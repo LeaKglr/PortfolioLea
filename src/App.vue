301da50a5b1fd73d50c150723a0346dd962c8cb8
LeaKglr <leakugler2@gmail.com>
2023-11-24 00:26:08 +0100

    Create page Accueil

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <header>
+  <header class="header">
     <NavBar />
   </header>
-  <h1>Hello</h1>
+  <main class="main">
+    <Accueil />
+  </main>
+  <footer class="footer"></footer>
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
+import Accueil from "./components/Accueil.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  width: 100%;
+  height: 100px;
+}
+
+.main {
+  width: 100%;
+  height: auto;
+  margin: 50px 0 30px 0;
+}
+
+.footer {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: flex-end;
+}
+</style>
--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,9 +1,129 @@
 <template>
-  <div></div>
+  <div id="main">
+    <div class="presentation">
+      <h1>Bienvenue ! 👋</h1>
+      <div class="NomPrenom">
+        <p>
+          Je me présente, je m'appelle {{ fullName }} mais vous pouvez aussi
+          m'appeler {{ nickNameCompleted }} !
+        </p>
+      </div>
+      <div class="formation">
+        <p>
+          Je suis actuellement en formation au Centre Européen de Formation
+          (CEF) afin d'obtenir mon titre de développeur web et web mobile (DWWM)
+          👩‍🎓
+        </p>
+      </div>
+      <div class="ContenuPortfolio">
+        <p>Vous trouverez dans ce portfolio :</p>
+        <ul>
+          <li>Ma présentation,</li>
+          <li>Mes créations,</li>
+          <li>Mon formulaire de contact.</li>
+        </ul>
+      </div>
+      <div class="rs">
+        <p>
+          N'hésitez pas à aller voir mes réseaux sociaux en bas de la page ! 😁
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="../assets/PhotoLeaKugler.png" alt="" />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { computed, ref } from "vue";
+
+const firstName = ref("Léa");
+const lastName = ref("Kugler");
+const nickName = ref("Dev");
+
+const fullName = computed({
+  get() {
+    return firstName.value + " " + lastName.value;
+  },
+});
+
+const nickNameCompleted = computed({
+  get() {
+    return firstName.value + " " + nickName.value;
+  },
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+#main {
+  width: 100%;
+  height: 500px;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+}
+
+p,
+li {
+  font-family: "Arimo", sans-serif;
+  color: black;
+  font-size: 20px;
+}
+
+li {
+  margin: 10px;
+}
+
+.presentation {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 30px;
+  padding: 0 0 20px 20px;
+  background-color: #d9d9d9;
+  border-radius: 5%;
+  border: solid #d9d9d9;
+  box-shadow: 0 0 30px #5de0e6;
+}
+
+h1 {
+  padding: 30px;
+  font-family: "Allerta Stencil", sans-serif;
+  color: black;
+  font-size: 50px;
+}
+
+.image {
+  width: 300px;
+  height: 450px;
+  display: flex;
+}
+
+.main img {
+  width: 100%;
+  height: 100%;
+  border-radius: 10%;
+  box-shadow: 0 0 30px #5de0e6;
+  background-color: #d9d9d9;
+}
+
+.NomPrenom {
+  margin-bottom: 20px;
+}
+
+.formation {
+  margin-bottom: 20px;
+}
+
+.ContenuPortfolio {
+  margin-right: 200px;
+}
+
+.rs {
+  margin-right: 200px;
+}
+</style>
