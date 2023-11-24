ca29c349b31d024a25ab7b6261ff310de53600c4
LeaKglr <leakugler2@gmail.com>
2023-11-24 01:15:17 +0100

    Add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,50 @@
 <template>
-  <div></div>
+  <div class="social-networks">
+    <a href="https://github.com/LeaKglr" target="_blank">
+      <img src="@/assets/ReseauxSociaux/logo github.png" alt="" />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/l%C3%A9a-kugler-24b640231/"
+      target="_blank"
+    >
+      <img src="@/assets/ReseauxSociaux/logo linkedin.png" alt="" />
+    </a>
+    <a
+      href="https://www.facebook.com/profile.php?id=100095303560501"
+      target="_blank"
+    >
+      <img src="@/assets/ReseauxSociaux/logo facebook.png" alt="" />
+    </a>
+    <a href="https://www.instagram.com/leadev2/" target="_blank">
+      <img src="@/assets/ReseauxSociaux/logo instagram.png" alt="" />
+    </a>
+  </div>
+  <div class="MAJDate">
+    <p class="date_maj">
+      Mise à jour du site le :
+      <time datetime="2023-21-11T18:00">21 Novembre 2023</time>
+    </p>
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.social-networks {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+}
+
+.social-networks img {
+  width: 60px;
+  height: 60px;
+}
+
+.date_maj {
+  font-family: "Arimo", sans-serif;
+}
+</style>
